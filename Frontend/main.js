d99074c27215ac3b24f1daf46370dbbb5e1cadfb
albertoz <zabeoalberto99@gmail.com>
2021-11-25 18:13:16 +0100

    Frontend: added docker-compose and personal area

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,6 +1,7 @@
 const blur = document.getElementById('blur');
 const radius = document.getElementById('radius');
 
+const zoom = 11;
 const vector = new ol.layer.Heatmap({
   title: "HeatMap",
   source: new ol.source.Vector({
@@ -17,26 +18,47 @@ const vector = new ol.layer.Heatmap({
   },
 });
 
+
 const raster = new ol.layer.Tile({
   title: "OSM",
   source: new ol.source.OSM(),
   opacity: 0.5
 });
 
+var view = new ol.View({
+  //projection: projection,
+  //units: "metric",
+  //extent: extent,
+  zoom: zoom,
+  maxZoom: 13,
+  minZoom: zoom - 1,
+  center: [ 1028954.9058868944, 5696879.17603661 ],
+  //numZoomLevels: 13
+});
+
 var map = new ol.Map({
+  controls: ol.control.defaults(),
   layers: [raster, vector],
   target: 'map',
-  view: new ol.View({
-    //projection: projection,
-    units: "metric",
-    //extent: extent,
-    zoom: 6,
-    maxZoom: 13,
-    minZoom: 3,
-    maxResolution: 6000,
-    center: [ 1028954.9058868944, 5696879.17603661 ],
-    //numZoomLevels: 13
-  })
+  view: view
+});
+
+var geocoder = new Geocoder('nominatim', {
+  provider: 'osm',
+  lang: 'en-US', //en-US, fr-FR
+  placeholder: 'Search for ...',
+  targetType: 'text-input',
+  autoComplete: 2,
+  autoCompleteTimeout: 200,
+  limit: 5,
+  keepOpen: true,
+  preventDefault: true
+});
+map.addControl(geocoder);
+geocoder.on('addresschosen', function(evt){
+  var feature = evt.feature,
+      coord = evt.coordinate;
+  map.getView().animate({ zoom: zoom, center: evt.coordinate });
 });
 
 /*
